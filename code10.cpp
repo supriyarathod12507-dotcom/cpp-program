aac0af600b3ab6b07fdc8b7b48bc7f691e79cd5e
supriyarathod12507-dotcom <supriyarathod12507@gmail.com>
2026-07-26 09:24:26 +0000

    change

---
+++ code10.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "Enter third number: ";
+    cin >> c;
+
+    if (a >= b && a >= c) {
+        cout << a << " is the largest number.";
+    }
+    else if (b >= a && b >= c) {
+        cout << b << " is the largest number.";
+    }
+    else {
+        cout << c << " is the largest number.";
+    }
+
+    return 0;
+}
